406f5c9c721218883044e049eff5cb138f476514
Tino Wang <tino5802@hotmail.com.tw>
2020-01-19 22:30:24 +0800

    usage of gorilla

--- 20200114_webWriteJSON.go
+++ 20200114_webWriteJSON.go
@@ -1,57 +1,45 @@
 package main
 
 import (
-	"fmt"
-	"encoding/json"
-	"log"
-	"net/http"
-	"strconv"
+//    "fmt"
+    "net/http"
+    "encoding/json"
+    "strconv"
+    "github.com/gorilla/mux"
 )
 
 type User struct {
-	Id      int
-	Name    string
-	Age     int
+    Id	int
+    Name	string
+    Age	int
 }
 
 var allUsers = []User {
-	{Id: 1,Name: "john", Age: 20},
-	{Id: 2,Name: "mary", Age: 18},
+    {Id: 1,Name: "john", Age: 20},
+    {Id: 2,Name: "mary", Age: 18},
 }
-
-
-
-//func jsonHandler (writer http.ResponseWriter, request *http.Request) {
-//	js, err := json.Marshal(allUsers[0])
-//	if err != nil {
-//		http.Error(writer, err.Error(), http.StatusInternalServerError)
-//		return
-//	}
-//
-//	writer.Header().Set("Content-Type", "application/json")
-//	writer.Write(js)
-//	fmt.Println(allUsers[0].Id)
-//}
 
 func main() {
-	for _, user := range allUsers {
-		path := "/"
-		path += strconv.Itoa(user.Id)
-		http.HandleFunc(path, func(w http.ResponseWriter, req *http.Request){
-			js, err := json.Marshal(allUsers[user.Id - 1])
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			
-			w.Header().Set("Content-Type", "application/json")
-			w.Write(js)
-		})
-		fmt.Println(path)
-	}
+    r := mux.NewRouter()
 
-	//http.HandleFunc("/json", jsonHandler)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+    r.HandleFunc("/users/{id}", func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        idURL := vars["id"]
+        id,_ := strconv.Atoi(idURL)
+        for _, user := range allUsers {
+            if user.Id == id {
+                js, err := json.Marshal(user)
+                if err != nil{
+                    http.Error(w, err.Error(), http.StatusInternalServerError)
+                    return
+                }
+
+                w.Header().Set("Content-Type","application/json")
+                w.Write(js)
+            }
+        }
+    })
+
+    http.ListenAndServe(":80", r)
 }
+
